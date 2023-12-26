5148243ac0764199b38640752d0a96e4f700bfd7
Diego Rocha <43792062+diegoRrocha221@users.noreply.github.com>
2023-12-26 13:59:02 -0300

    Update cloner.sh

--- cloner.sh
+++ cloner.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 
-# Defina o diretório onde você deseja clonar o repositório
-diretorio_clonagem="/caminho/para/o/repositorio"
+clone_dir="/path/to/clone"
 
-# Defina a URL SSH do repositório Git que você deseja clonar
-url_repositorio="git@github.com:seu-usuario/seu-repositorio.git"
+url_repo="git@github.com:git-user/repository.git"
 
-# Verifique se o diretório de clonagem já existe. Se não existir, crie-o.
-if [ ! -d "$diretorio_clonagem" ]; then
-  mkdir -p "$diretorio_clonagem"
+if [ ! -d "$clone_dir" ]; then
+  mkdir -p "$clone_dir"
 fi
 
-# Mude para o diretório de clonagem
-cd "$diretorio_clonagem"
+cd "$clone_dir"
 
-# Verifique se já foi feito um clone hoje (verificando se o arquivo 'last_clone.txt' existe e tem a data atual)
 if [ -f "last_clone.txt" ] && [ "$(date +%F)" = "$(cat last_clone.txt)" ]; then
-  echo "Já foi feito um clone hoje. O script será encerrado."
+  echo "clone start"
   exit 0
 fi
 
-# Clone o repositório via SSH
-git clone "$url_repositorio"
+git clone "$url_repo"
 
-# Salve a data atual no arquivo 'last_clone.txt'
 date +%F > last_clone.txt
 
-echo "Clone concluído com sucesso."
+echo "Get repo complete"
